b2debfd7cca8eaed8c77938430a19154359af01f
Taseng Mancheykhun <taseng9@gmail.com>
2024-10-27 23:34:49 +0530

    day1_4

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="hisabkitab",
+    version="1.0.0",
+    description="A simple CLI tool to keep track of your payment dues",
+    author="Taseng Mancheykhun",
+    author_email="taseng9@gmail.com",
+    url="https://github.com/TasengMancheykhun/hisabkitab",
+    py_modules=["hisabkitab"],
+    entry_points={
+        "console_scripts": [
+            "hisab=kitab:main",
+        ],
+    },
+    install_requires=[
+        "tabulate",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
